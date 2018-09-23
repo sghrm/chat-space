671ea3007160e30073f72f107cb2023b08780201
sghrm <sugaharamasaki0530@gmail.com>
2018-09-23 20:13:51 +0900

    ビュー、部分テンプレート、コントローラーの編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 	def index
 	end
-	
+
 	def new
 	  @group = Group.new
 	  @group.users << current_user
@@ -16,14 +16,24 @@ class GroupsController < ApplicationController
 	  end
 	end
 
-	def edit
+	def update
+	  if @group.update(group_params)
+	  	redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+	  else
+	  	render :edit
+	  end
 	end
 
-	def update
+	def edit
+		@group = Group.new
 	end
 
 	private
 	def group_params
 	  params.require(:group).permit(:name, {:user_ids => []})
+	end
+
+	def set_group
+	  @group = Group.find(params[:id])
 	end
 end
