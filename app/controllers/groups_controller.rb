ae43e9085c44a31a6ee0838f88f13582576ff723
sghrm <sugaharamasaki0530@gmail.com>
2018-09-23 18:50:30 +0900

    グループ作成機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,16 @@
 class GroupsController < ApplicationController
 	def new
+	  @group = Group.new
+	  @group.users << current_user
+	end
+
+	def create
+	  @group = Group.new(group_params)
+	  if @group.save
+	  	redirect_to root_path, notice: 'グループを作成しました'
+	  else
+	  	render :new
+	  end
 	end
 
 	def edit
@@ -8,8 +19,11 @@ class GroupsController < ApplicationController
 	def update
 	end
 
-	def create
-	end
 	def index
+	end
+
+	private
+	def group_params
+	  params.require(:group).permit(:name, {:user_ids => []})
 	end
 end
