3d5a56187673573c853d24e416f962fde9f7c7ec
sghrm <sugaharamasaki0530@gmail.com>
2018-09-29 19:56:54 +0900

    FctoryGirl-setting

---
+++ spec/factories/groups.rb
@@ -0,0 +1,5 @@
+FctoryGirl.define do
+	factory :group do
+		name Faker::Team.name
+	end
+end
---
+++ spec/factories/messages.rb
@@ -0,0 +1,9 @@
+FctoryGirl.define do
+
+	factory :message do
+		content    Faker::Lorem.sentence
+		image      File.open("#{Rails.root}/public/images/no_image.jpg")
+		user
+		group
+	end
+end
