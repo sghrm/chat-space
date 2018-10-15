691b19408d30371cb2f90b6a6b64f1ed9e4c59a2
sghrm <sugaharamasaki0530@gmail.com>
2018-10-15 14:56:14 +0900

    非同期通信実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(function(){
+	function buildHTML(message){
+		var html = `<div class = "chat-main__body--messages-list">
+						<div class = "chat-main__message clearfix">
+							<div class ="chat-main__message-name">
+								<p>
+									${message.name}
+								</p>
+							</div>
+							<div class ="chat-main__message-time">
+								<p>
+									${message.created_at}
+								</p>
+							</div>
+							<div class ="chat-main__message-body">
+								<p>
+									${message.content}
+								</p>
+							</div>
+						</div>
+					</div>`
+		return html;
+	}
+	//非同期通信
+	$('#new_message').on('submit' , function(e){
+		e.preventDefault();
+		var formData = new FormData(this);
+		var url = $(this).attr('action');
+		$.ajax({
+			url: url,
+			type: "POST",
+			data: formData,
+			dataType: 'json',
+			processData: false,
+			contentType: false
+		})
+		.done(function(message){
+			var html = buildHTML(message);
+			$('.chat-main__body').append(html) 
+			$('.message').val('')
+		})
+		.fail(function(){
+			alert("エラーが発生しました");
+		})
+	})
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.content @message.content
+json.name @message.user.name
+json.created_at @message.created_at
+json.image @message.image
+json.id @message.id
