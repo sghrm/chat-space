adc1751b281cebc3cad249ad23a557f64ab8698e
sghrm <sugaharamasaki0530@gmail.com>
2018-10-17 21:29:39 +0900

    disabledの修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -38,6 +38,7 @@ $(function(){
 			var html = buildHTML(message);
 			$('.chat-main__body').append(html) 
 			$('.message').val('')
+			$('.submit').prop('disabled', false);
 			$('.chat-main__body').animate( {'scrollTop': $('.chat-main__body')[0].scrollHeight}, 'fast' );
 		})
 		.fail(function(){
